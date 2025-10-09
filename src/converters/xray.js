d8bd0c40df98ea6ee69168040b98bae1eaa184c1
中国特色社会主义码农 <air2316361@sina.com>
2025-10-09 09:43:03 +0800

    1

--- src/converters/xray.js
+++ src/converters/xray.js
@@ -2,10 +2,10 @@ export default function(data) {
 	const proxies = data.outbounds;
 	const result = [];
 	for (const proxy of proxies) {
-		if (proxy.tag !== "proxy") {
+		if (proxy.tag !== 'proxy') {
 			continue;
 		}
-		const protocol =  proxy.protocol;
+		const protocol = proxy.protocol;
 		const vnexts = proxy.settings.vnext;
 		const streamSettings = proxy.streamSettings;
 		const realitySettings = streamSettings.realitySettings;
@@ -13,19 +13,22 @@ export default function(data) {
 			const address = vnext.address;
 			const user = vnext.users[0];
 			result.push({
-				"type": protocol,
-				"server": address,
-				"port": vnext.port,
-				"uuid": user.id,
-				"network": streamSettings.network,
-				"tls": true,
-				"flow": user.flow,
-				"servername": realitySettings.serverName,
-				"reality-opts": {
-					"public-key": realitySettings.publicKey,
-					"short-id": realitySettings.shortId
+				'type': protocol,
+				'server': address,
+				'port': vnext.port,
+				'uuid': user.id,
+				'flow': 'xtls-rprx-vision',
+				'packet-encoding': 'xudp',
+				'tls': true,
+				'servername': realitySettings.serverName,
+				'alpn': ['h2', 'http/1.1'],
+				'client-fingerprint': realitySettings.fingerprint,
+				"skip-cert-verify": true,
+				'reality-opts': {
+					'public-key': realitySettings.publicKey,
+					'short-id': realitySettings.shortId
 				},
-				"client-fingerprint": realitySettings.fingerprint
+				'network': streamSettings.network
 			});
 		}
 	}
