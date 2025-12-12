e51cbb1767797d8e9a9218996bd9846c75e4862b
中国特色社会主义码农 <air2316361@sina.com>
2025-12-12 11:23:40 +0800

    1

--- src/converters/hysteria2.js
+++ src/converters/hysteria2.js
@@ -1,31 +1,25 @@
 export default function(data) {
 	const server = data.server;
-	let address;
-	let port;
-	if (server.startsWith('[')) {
-		const serverSplit = server.split(']');
-		address = serverSplit[0].substring(1);
-		port = serverSplit[1].substring(1);
-	} else {
-		const serverSplit = server.split(':');
-		address = serverSplit[0];
-		port = serverSplit[1];
+	const index = server.lastIndexOf(':');
+	let address = server.substring(0, index);
+	if (address.startsWith('[')) {
+		address = address.substring(1, address.length - 1);
 	}
+	const port = server.substring(index + 1);
 	const tls = data.tls;
 	const proxy = {
-		"type": "hysteria2",
-		"server": address,
-		"password": data.auth,
-		"sni": tls.sni,
-		"skip-cert-verify": tls.insecure,
-		"protocol": data.protocol,
-		"recv-window-conn": data["recv_window_conn"],
-		"recv-window": data["recv_window"],
-	}
-	const portSplit = server[1].split(',');
+		'type': 'hysteria2',
+		'server': address,
+		'password': data.auth,
+		'sni': tls.sni,
+		'skip-cert-verify': tls.insecure,
+		'protocol': data.protocol,
+		'recv-window-conn': data['recv_window_conn'],
+		'recv-window': data['recv_window']
+	};
+	const portSplit = port.split(',');
 	proxy.port = portSplit[0];
 	if (portSplit.length > 1) {
-	} else {
 		proxy.ports = portSplit[1];
 	}
 	return [proxy];
