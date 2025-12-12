868dcdf84ecd05551fe2a1bcd0c3e86ef1e0abec
中国特色社会主义码农 <air2316361@sina.com>
2025-12-12 08:48:58 +0800

    1

--- src/converters/hysteria.js
+++ src/converters/hysteria.js
@@ -1,18 +1,21 @@
 export default function(data) {
 	const server = data.server;
 	const index = server.lastIndexOf(':');
-	const address = server.substring(0, index);
+	let address = server.substring(0, index);
+	if (address.startsWith('[')) {
+		address = address.substring(1, address.length - 1);
+	}
 	const port = server.substring(index + 1);
 	const proxy = {
-		"type": "hysteria",
-		"server": address,
-		"auth-str": data["auth_str"],
-		"sni": data["server_name"],
-		"skip-cert-verify": data["disable_mtu_discovery"],
-		"protocol": data.protocol,
-		"recv-window-conn": data["recv_window_conn"],
-		"recv-window": data["recv_window"],
-	}
+		'type': 'hysteria',
+		'server': address,
+		'auth-str': data['auth_str'],
+		'sni': data['server_name'],
+		'skip-cert-verify': data['disable_mtu_discovery'],
+		'protocol': data.protocol,
+		'recv-window-conn': data['recv_window_conn'],
+		'recv-window': data['recv_window']
+	};
 	proxy.port = port[0];
 	if (port.length > 1) {
 	} else {
